08f7bf757019d5c1b4f61af0ca06138643f1097d
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-05-26 17:57:22 +0100

    Make tests pass

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
-using Base.Broadcast: broadcastable
-using BenchmarkTools
 using ChainRulesCore
 using ChainRulesOverloadGeneration
+# resolve conflicts while this code exists in both.
+const on_new_rule = ChainRulesOverloadGeneration.on_new_rule
+const refresh_rules = ChainRulesOverloadGeneration.refresh_rules
+
 using Test
 
 @testset "ChainRulesCore" begin
